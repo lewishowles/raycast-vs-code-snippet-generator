bfe9527662f5670f80e858e6e8d2fc0bee2ea16b
Lewis Howles <lewishowles@users.noreply.github.com>
2025-08-04 15:35:11 +0100

    Create the basic form, mimicking the online tool
    This includes familiarisation with the form fields and how they work.

--- src/create-vs-code-snippet.tsx
+++ src/create-vs-code-snippet.tsx
@@ -1,40 +1,30 @@
 import { Form, ActionPanel, Action, showToast } from "@raycast/api";
 
-type Values = {
-  textfield: string;
-  textarea: string;
-  datepicker: Date;
-  checkbox: boolean;
-  dropdown: string;
-  tokeneditor: string[];
+type FormValues = {
+	title: string;
+	prefix: string;
+	code: string;
 };
 
 export default function Command() {
-  function handleSubmit(values: Values) {
-    console.log(values);
-    showToast({ title: "Submitted form", message: "See logs for submitted values" });
-  }
+	function generateSnippet(formValues: FormValues) {
+		console.log(formValues);
+		showToast({ title: "Submitted form", message: "See logs for submitted values" });
+	}
 
-  return (
-    <Form
-      actions={
-        <ActionPanel>
-          <Action.SubmitForm onSubmit={handleSubmit} />
-        </ActionPanel>
-      }
-    >
-      <Form.Description text="This form showcases all available form elements." />
-      <Form.TextField id="textfield" title="Text field" placeholder="Enter text" defaultValue="Raycast" />
-      <Form.TextArea id="textarea" title="Text area" placeholder="Enter multi-line text" />
-      <Form.Separator />
-      <Form.DatePicker id="datepicker" title="Date picker" />
-      <Form.Checkbox id="checkbox" title="Checkbox" label="Checkbox Label" storeValue />
-      <Form.Dropdown id="dropdown" title="Dropdown">
-        <Form.Dropdown.Item value="dropdown-item" title="Dropdown Item" />
-      </Form.Dropdown>
-      <Form.TagPicker id="tokeneditor" title="Tag picker">
-        <Form.TagPicker.Item value="tagpicker-item" title="Tag Picker Item" />
-      </Form.TagPicker>
-    </Form>
-  );
+	return (
+		<Form
+			actions={
+				<ActionPanel>
+					<Action.SubmitForm title="Generate Snippet" onSubmit={generateSnippet} />
+				</ActionPanel>
+			}
+		>
+			<Form.Description text="Enter your code snippet to convert it." />
+			<Form.TextField id="title" title="Title" />
+			<Form.TextField id="prefix" title="Prefix" info="For multiple prefixes, please comma separate them." />
+			<Form.Separator />
+			<Form.TextArea id="code" title="Code" />
+		</Form>
+	);
 }
